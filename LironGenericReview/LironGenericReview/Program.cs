8385110ed2d551511e815cdae94214029bcfaa21
Lion Saedi <lionsaedi28@outlook.com>
2020-08-19 17:36:06 -0700

    I started the generics project. TO DO: Finsih the functions for th remove

---
+++ LironGenericReview/LironGenericReview/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace LironGenericReview
+{
+    
+    class Program
+    {
+
+        //Make a generic list
+        //Generic array inside the list. 
+        //Array has a minimum capacity  
+        //Add, Remove, RemoveAt, count, indexing
+        //You need the List to have the following interfaces:
+        //IList<T>
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ LironGenericReview/LironGenericReview/myList.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LironGenericReview
+{
+    class myList<T> : IList<T>
+    {
+        T[] data;
+        int minimumCapacity;
+        public myList(int minimumCapacity)
+        {
+            this.minimumCapacity = minimumCapacity;
+            data = new T[minimumCapacity];
+            Count = 0;
+        }
+
+
+        public T this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int Count { get; set; }
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public void Add(T item)
+        {
+            if (!(data.Length == Count))
+            {
+                data[Count] = item;
+            }
+
+            else
+            {
+                //cerate a temporary array for loop it
+            }
+
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
